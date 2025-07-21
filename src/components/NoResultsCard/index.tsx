04aadd589559a87bb0df1ddaf2738a25ff988371
Natalia Bello <nathsep9@gmail.com>
2025-07-20 22:15:31 -0500

    feat: add NoResultsCard component with styles and content

---
+++ src/components/NoResultsCard/index.tsx
@@ -0,0 +1,32 @@
+import styles from './NoResultsCard.module.scss';
+
+const NoResultsCard = () => {
+  return (
+    <div className={styles.rootNoResults}>
+      <div className={styles.card}>
+        <img
+          src="https://http2.mlstatic.com/frontend-assets/search-nordic/not-found.svg"
+          alt="No results found"
+          className={styles.image}
+        />
+        <div className={styles.text}>
+          <h2>No hay publicaciones que coincidan con tu búsqueda.</h2>
+          <ul className={styles.suggestions}>
+            <li>
+              <strong>Revisa la ortografía</strong> de la palabra.
+            </li>
+            <li>
+              <strong>Utiliza palabras</strong> más genéricas o menos palabras.
+            </li>
+            <li>
+              <span className={styles.link}>Navega por las categorías</span> para encontrar un
+              producto similar
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoResultsCard;
